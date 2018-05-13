e6c567c7abee0d4727e091986e73aaa015461a0f
Roscoe Sabale <rsabale43@gmail.com>
2018-05-13 19:44:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,21 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var obj = {};
+  var min = Math.ceil(0);
+  var max = Math.floor(100);
+  var price = Math.floor(Math.random() * (max - min + 1)) + min;
+  obj[item] = price;
+  cart.push(obj);
+  console.log(`${item} has been added to your cart.`);
+  return cart;
 }
 
 function viewCart() {
   // write your code here
+  if (cart.length == 0) {
+    return 'Your shopping cart is empty.';
+  }
 }
 
 function total() {
